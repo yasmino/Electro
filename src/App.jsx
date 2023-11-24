70f30df6c108cf1868999fec01cf7eda0656b8fa
Noha-Thabet <nohaathabet@gmail.com>
2023-11-24 21:34:56 +0100

    chore(electro): remove unnessecary code

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-    <p>HEllo</p>
+      <h1>Electro App</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
