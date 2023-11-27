30655d6ace8f32b0a56b6d306e9b3ebd3ababa45
Noha-Thabet <nohaathabet@gmail.com>
2023-11-27 13:46:28 +0100

    feat(Navbar): create Basic Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import NavBar from "./Components/Navbar/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import Cart from "./Pages/Cart";
+import WhishList from "./Pages/WhishList";
 
 function App() {
   return (
-    <>
-      <NavBar />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/whishList" element={<WhishList />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
