3da7a669280d5ac23057bc96d2fbdb38bcd7a845
Yasmin Masoud <yasminoreda@gmail.com>
2023-11-29 16:21:09 +0100

    feat(category-section): create category section ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
+import CategorySection from './components/category-section/CategorySection';
 import CategoryBar from './components/category-bar/CategoryBar';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Mobile from "./pages/Mobile";
@@ -9,18 +10,19 @@ import Tablets from './pages/Tablets';
 
 function App() {
   return (
-    <React.Fragment>
+  <React.Fragment>
     <CssBaseline />
     <BrowserRouter>
-    <CategoryBar />
+      <CategoryBar />
       <Routes>
         <Route path="/mobile" element={<Mobile />} />
         <Route path="/laptop" element={<Laptop />} />
         <Route path="/smartphone" element={<Smartwatch />} />
         <Route path="/tablets" element={<Tablets />} />
       </Routes>
-      </BrowserRouter>
-    </React.Fragment>
+      <CategorySection />
+    </BrowserRouter>
+  </React.Fragment>
    
   );
 }
