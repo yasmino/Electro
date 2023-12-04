85f04e4930685b926fc6aaced80e87cc5f18d4e6
Noha-Thabet <nohaathabet@gmail.com>
2023-12-04 20:44:24 +0100

    feat(mobiles):create simple UI structure for mobile component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import * as React from 'react';
-import CssBaseline from '@mui/material/CssBaseline';
+import * as React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Mobile from "./Pages/Mobile";
-import Laptop from './Pages/Laptop';
-import Smartwatch from './Pages/Smartwatch';
-import Tablets from './Pages/Tablets';
+import Laptop from "./Pages/Laptop";
+import Smartwatch from "./Pages/Smartwatch";
+import Tablets from "./Pages/Tablets";
 import Home from "./Pages/Home";
 import Login from "./Pages/Login";
 import Cart from "./Pages/Cart";
 import WishList from "./Pages/WishList";
-import './App.css'
+import "./App.css";
 
 function App() {
   return (
-  <React.Fragment>
-    <CssBaseline />
-    <BrowserRouter>
-      <Routes>
-        <Route path="/mobile" element={<Mobile />} />
-        <Route path="/laptop" element={<Laptop />} />
-        <Route path="/smartphone" element={<Smartwatch />} />
-        <Route path="/tablets" element={<Tablets />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/wishList" element={<WishList />} />
-      </Routes>
-
-    </BrowserRouter>
-  </React.Fragment>
+    <React.Fragment>
+      <CssBaseline />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/mobile" element={<Mobile />} />
+          <Route path="/laptop" element={<Laptop />} />
+          <Route path="/smartphone" element={<Smartwatch />} />
+          <Route path="/tablets" element={<Tablets />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/wishList" element={<WishList />} />
+        </Routes>
+      </BrowserRouter>
+    </React.Fragment>
   );
 }
 
-export default App
+export default App;
