0d6604346e0d902fe1fba5146e81f3b402fd199d
Noha-Thabet <nohaathabet@gmail.com>
2023-12-04 22:32:39 +0100

    feat(style-Mobile-section)

--- src/Components/Mobile/Mobiles.jsx
+++ src/Components/Mobile/Mobiles.jsx
@@ -1,32 +1,15 @@
 import { useState, useEffect } from "react";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
-import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import "../category-section/Category.css";
 import Grid from "@mui/material/Grid";
-import AddShoppingCartOutlinedIcon from "@mui/icons-material/AddShoppingCartOutlined";
-import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
-import ArrowForwardOutlinedIcon from "@mui/icons-material/ArrowForwardOutlined";
-import Rating from "@mui/material/Rating";
 import data from "../../../Data/db.json";
+import MobileList from "./MobileList";
+import ArrowForwardOutlinedIcon from "@mui/icons-material/ArrowForwardOutlined";
+import PropTypes from "prop-types";
 
 const Mobiles = () => {
-  const [mobileData, setMobileDate] = useState([]);
-
-  const Electronics = data.Electronics;
-
-  const fetchMobileData = () => {
-    const mobiles = Electronics.filter(
-      (electronic) => electronic.type === "Mobile"
-    );
-    setMobileDate(mobiles);
-  };
-
-  useEffect(() => {
-    fetchMobileData();
-  }, []);
-
   function Item(props) {
     const { sx, ...other } = props;
     return (
@@ -63,6 +46,21 @@ const Mobiles = () => {
     ]),
   };
 
+  const [mobileData, setMobileDate] = useState([]);
+
+  const Electronics = data.Electronics;
+
+  const fetchMobileData = () => {
+    const mobiles = Electronics.filter(
+      (electronic) => electronic.type === "Mobile"
+    );
+    setMobileDate(mobiles);
+  };
+
+  useEffect(() => {
+    fetchMobileData();
+  }, []);
+
   return (
     <>
       <Container maxWidth="lg" sx={{ height: "100vh" }}>
@@ -79,46 +77,18 @@ const Mobiles = () => {
           {mobileData.slice(0, 4).map((data, index) => {
             const { name, image, price } = data;
             return (
-              <Grid key={index} item xs={6} md={3} spacing={3}>
-                <Item className="product-image">
-                  <div className="wishlist">
-                    <FavoriteBorderOutlinedIcon />
-                  </div>
-                  <img src={image} />
-                </Item>
-                <div className="ratings-container">
-                  <Rating
-                    name="half-rating"
-                    defaultValue={2.5}
-                    precision={0.5}
-                  />
-                </div>
-                <Item className="product">
-                  <Box className="product-details">
-                    <Item sx={{ flexGrow: 1 }}>
-                      <h3 className="product-title">{name}</h3>
-                    </Item>
-                    <Item className="cart-icon">
-                      <AddShoppingCartOutlinedIcon />
-                    </Item>
-                  </Box>
-
-                  <div className="product-price">${price}</div>
-                </Item>
-              </Grid>
+              <MobileList key={index} name={name} image={image} price={price} />
             );
           })}
 
           <Box className="discover-more">
             <Item sx={{ flexGrow: 3 }}>
-              <Link to="/" style={{ textDecoration: "none" }}>
+              <Link to="/mobile" style={{ textDecoration: "none" }}>
                 <h3 className="discover-more-title">Discover more</h3>
               </Link>
             </Item>
             <Item className="arrow-icon">
-              <Link to="/">
-                <ArrowForwardOutlinedIcon />
-              </Link>
+              <ArrowForwardOutlinedIcon />
             </Item>
           </Box>
         </Grid>
