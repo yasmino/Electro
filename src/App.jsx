2138dc09137985c7b3642a4d54fdf965f150584b
Yasmin Masoud <yasminoreda@gmail.com>
2023-12-05 12:35:05 +0100

    fix conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
-import * as React from 'react';
-import CssBaseline from '@mui/material/CssBaseline';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import * as React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import { Route, Routes } from "react-router-dom";
 import Mobile from "./Pages/Mobile";
-import Laptop from './Pages/Laptop';
-import Smartwatch from './Pages/Smartwatch';
-import Tablets from './Pages/Tablets';
+import Laptop from "./Pages/Laptop";
+import Smartwatch from "./Pages/Smartwatch";
+import Tablets from "./Pages/Tablets";
 import Home from "./Pages/Home";
 import Login from "./Pages/Login";
 import Cart from "./Pages/Cart";
 import WishList from "./Pages/WishList";
-import './App.css'
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import CategoryBar from "./Components/category-bar/CategoryBar";
 
 function App() {
   return (
-  <React.Fragment>
-    <CssBaseline />
-    <BrowserRouter>
+    <React.Fragment>
+      <CssBaseline />
       <Routes>
-        <Route path="/mobile" element={<Mobile />} />
-        <Route path="/laptop" element={<Laptop />} />
-        <Route path="/smartphone" element={<Smartwatch />} />
-        <Route path="/tablets" element={<Tablets />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/wishList" element={<WishList />} />
+        <Route path="/" element={<Navbar />}>
+          <Route element={<CategoryBar />}>
+            <Route index element={<Home />} />
+            <Route path="mobile" element={<Mobile />} />
+            <Route path="laptop" element={<Laptop />} />
+            <Route path="smartphone" element={<Smartwatch />} />
+            <Route path="tablets" element={<Tablets />} />
+            <Route path="login" element={<Login />} />
+            <Route path="cart" element={<Cart />} />
+            <Route path="wishList" element={<WishList />} />
+          </Route>
+        </Route>
       </Routes>
-
-    </BrowserRouter>
-  </React.Fragment>
+    </React.Fragment>
   );
 }
 
-export default App
+export default App;
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import "./navbar.css";
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import {
   AppBar,
   Box,
@@ -17,75 +17,96 @@ import RegistrationForm from "../registration/registrationform";
 
 const Navbar = () => {
   return (
-    <AppBar position="static" sx={{ backgroundColor: "white" }}>
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-
-          <Grid container sx={{ p: 1 }} className="Nav-Bar">
-            {/* logo */}
-            <Grid item xs={3} md={4}>
-              <Typography
-                variant="h4"
-                className="electroLogo"
-                sx={{
-                  color: "black",
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "space-between",
-                  justifyContent: "center",
-                  lineHeight: "2",
-                }}
-              >
-                Electro
-              </Typography>
-            </Grid>
-
-            {/* search */}
-            <Grid item xs={6} md={4}>
-              <Box
-                sx={{
-                  display: "flex",
-                  flexDirection: "column",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  height: "70px",
-                }}
-              >
-                <TextField
-                  id="outlined-search"
-                  label="Search "
-                  type="search"
-                  className="searchBox"
-                  sx={{ width: "350px" }}
-                />
-              </Box>
-            </Grid>
-
-            {/* pages */}
-            <Grid item xs={3} md={4}>
-              <Box className="nav-icons" sx={{ display: { xs: "none", md: "flex" }}}>
-                <Button className="nav-btn carticon">
-                  <Link to="/cart">
-                    <ShoppingCartIcon sx={{ color: "black" }} />
-                    <span className="cart-count">0</span>
+    <>
+      <AppBar position="static" sx={{ backgroundColor: "white" }}>
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <Grid container sx={{ p: 1 }} className="Nav-Bar">
+              {/* logo */}
+              <Grid item xs={3} md={4}>
+                <Typography
+                  variant="h4"
+                  className="electroLogo"
+                  sx={{
+                    color: "black",
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "space-between",
+                    justifyContent: "center",
+                    lineHeight: "2",
+                  }}
+                >
+                  <Link to="/" className="logoLink">
+                    Electro
                   </Link>
-                </Button>
+                </Typography>
+              </Grid>
 
-                <Button className="nav-btn registericon">
+              {/* search */}
+              <Grid item xs={6} md={4}>
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    height: "70px",
+                  }}
+                >
+                  <TextField
+                    id="outlined-search"
+                    label="Search "
+                    type="search"
+                    className="searchBox"
+                    sx={{ width: "350px" }}
+                  />
+                </Box>
+              </Grid>
+
+              {/* pages */}
+              <Grid item xs={3} md={4}>
+                <Box
+                  className="nav-icons"
+                  sx={{
+                    flexGrow: 1,
+                    display: { xs: "none", md: "flex" },
+                    flexDirection: "row",
+                    justifyContent: "end",
+                    alignItems: "center",
+                  }}
+                >
+                  {/* cart */}
+                  <Button
+                    className="nav-btn"
+                    sx={{
+                      my: 2,
+                      display: "block",
+                    }}
+                  >
+                    <Link to="/cart">
+                      <ShoppingCartIcon sx={{ color: "black" }} />
+                    </Link>
+                  </Button>
+
+                  {/* login */}
+                  <Button className="nav-btn registericon">
                   <RegistrationForm />
                 </Button>
 
-                <Button className="nav-btn wishlisticon">
+                  {/* wishList */}
+                  <Button className="nav-btn wishlisticon">
                   <Link to="/wishList">
                     <FavoriteIcon sx={{ color: "black" }} />
                   </Link>
                 </Button>
-              </Box>
+                </Box>
+              </Grid>
             </Grid>
-          </Grid>
-        </Toolbar>
-      </Container>
-    </AppBar>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      <Outlet />
+    </>
   );
 };
 
