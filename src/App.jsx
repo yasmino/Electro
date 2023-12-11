c06da3df2f25800b014ba2ee6bfe0d3e0279232d
Yasmin Masoud <yasminoreda@gmail.com>
2023-12-11 23:52:28 +0100

    fix(routes): fix pages routes

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,12 @@ import Laptop from "./Pages/Laptop";
 import Smartwatch from "./Pages/Smartwatch";
 import Tablets from "./Pages/Tablets";
 import Home from "./Pages/Home";
-import Login from "./Pages/Login";
 import Cart from "./Pages/Cart";
-import WishList from "./Pages/WishList";
+import Wishlist from "./WishlistContext/wishlist";
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
 import CategoryBar from "./Components/category-bar/CategoryBar";
+
 
 function App() {
   return (
@@ -23,11 +23,10 @@ function App() {
             <Route index element={<Home />} />
             <Route path="mobile" element={<Mobile />} />
             <Route path="laptop" element={<Laptop />} />
-            <Route path="smartphone" element={<Smartwatch />} />
+            <Route path="smartwatch" element={<Smartwatch />} />
             <Route path="tablets" element={<Tablets />} />
-            <Route path="login" element={<Login />} />
             <Route path="cart" element={<Cart />} />
-            <Route path="wishList" element={<WishList />} />
+            <Route path="wishlist" element={<Wishlist />} />
           </Route>
         </Route>
       </Routes>
