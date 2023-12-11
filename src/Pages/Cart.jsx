38ec7bb46aae52dbaf891de038e2af1f653dbdc7
Noha-Thabet <nohaathabet@gmail.com>
2023-12-11 18:42:43 +0100

    feat(subAmount):get subTotal  for each element

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -5,16 +5,21 @@ const Cart = () => {
   const { removeFromCartIcon, addToCartHandler } = useContext(CartContext);
   // get items from local storage
   const items = JSON.parse(localStorage.getItem("cartItems")) || [];
+
   return (
     <>
       {items.length === 0 && <h1>No Founded Items</h1>}
+
       <div>
         {items.map((item, index) => {
+          const subTotal = item.quantity * item.price.toFixed(2);
+
           return (
             <div key={index}>
               <h2>{item.name}</h2>
               <h3>{item.price}</h3>
               <p>{item.quantity}</p>
+              <h3>Totao Price : ${subTotal}</h3>
               <button
                 onClick={() => {
                   removeFromCartIcon(item);
