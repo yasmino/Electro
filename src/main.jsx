9ebe8ffcd341afe86e4b26ac0750cb2dd3ed44ce
Yasmin Masoud <yasminoreda@gmail.com>
2023-12-11 23:53:33 +0100

    feat(wishlist): wrap app with  WishlistProvider

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import CartContextProvider from "./Components/Context/CartContext.jsx";
+import WishlistProvider from './WishlistContext/WishlistProvider';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <CartContextProvider>
+      <WishlistProvider>
         <App />
-      </CartContextProvider>
+    </WishlistProvider>
+    </CartContextProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
