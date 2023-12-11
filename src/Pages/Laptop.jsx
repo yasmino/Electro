36aabf0c5b112f6d8f324fff0c4eaaa9c40a2368
Yasmin Masoud <yasminoreda@gmail.com>
2023-12-11 23:49:20 +0100

    feat(pages): fetch data in separate category pages

--- src/Pages/Laptop.jsx
+++ src/Pages/Laptop.jsx
@@ -1,9 +1,48 @@
-const Laptop = () => {
-  return (
-    <div>
-      Laptop Page
-    </div>
-  )
-}
+import "../Components/category-section/Category.css"
+import LaptopList from "../Components/Laptop/LaptopList"
+import { useContext, useEffect } from "react";
+import { WishlistContext } from "../WishlistContext/WishlistProvider";
+import Grid from "@mui/material/Grid";
+import Container from '@mui/material/Container';
 
-export default Laptop
+const Smartwatch = () => {
+
+  const {Electronics,laptopData, setLaptopData} = useContext(WishlistContext);
+
+  const fetchTabletData = () => {
+    const Laptop = Electronics.filter(
+      (electronic) => electronic.type === "Laptop"
+    );
+    setLaptopData(Laptop);
+  };
+
+  useEffect(() => {
+    fetchTabletData();
+  }, []);
+
+  return (
+   <>
+     <div className="category-section">
+        <div className="category-title">
+          <div className="divide-line"></div>
+          <span>
+            <strong>Laptop</strong> Products
+          </span>
+          <div className="divide-line"></div>
+        </div>
+      </div>
+      <Container maxWidth="lg" sx={{ height: "100vh" }}>
+      <Grid container spacing={2} className="product-section">
+        {laptopData.map((data, index) => {
+          const { id, name, image, price } = data;
+          return (
+            <LaptopList key={index} name={name} image={image} price={price} id={id} />
+          );
+        })}
+      </Grid>
+      </Container>
+</>
+  );
+};
+
+export default Smartwatch;
--- src/Pages/Tablets.jsx
+++ src/Pages/Tablets.jsx
@@ -1,9 +1,48 @@
-const Tablets = () => {
-  return (
-    <div>
-      Tablet Page
-    </div>
-  )
-}
+import "../Components/category-section/Category.css"
+import TabletList from "../Components/Tablet/TabletList"
+import { useContext, useEffect } from "react";
+import { WishlistContext } from "../WishlistContext/WishlistProvider";
+import Grid from "@mui/material/Grid";
+import Container from '@mui/material/Container';
 
-export default Tablets
+const Tablet = () => {
+
+  const {Electronics, setTabletData, tabletData} = useContext(WishlistContext);
+
+  const fetchTabletData = () => {
+    const tablets = Electronics.filter(
+      (electronic) => electronic.type === "Tablet"
+    );
+    setTabletData(tablets);
+  };
+
+  useEffect(() => {
+    fetchTabletData();
+  }, []);
+
+  return (
+   <>
+      <div className="category-section">
+        <div className="category-title">
+          <div className="divide-line"></div>
+          <span>
+            <strong>Tablets</strong> Products
+          </span>
+          <div className="divide-line"></div>
+        </div>
+      </div>
+      <Container maxWidth="lg" sx={{ height: "100vh" }}>
+      <Grid container spacing={2} className="product-section">
+        {tabletData.map((data, index) => {
+          const { id, name, image, price } = data;
+          return (
+            <TabletList key={index} name={name} image={image} price={price} id={id} />
+          );
+        })}
+      </Grid>
+      </Container>
+</>
+  );
+};
+
+export default Tablet;
